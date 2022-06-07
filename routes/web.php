a4b3a87201f622ceb8e43ca2dde53db4af83c5c9
VincenzoBorriello89 <vincborr89@gmail.com>
2022-06-07 16:04:41 +0200

    install auth e table migrate ok

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+// localhost: 8080/home
+Route::get('/home', 'HomeController@index')->name('home');
